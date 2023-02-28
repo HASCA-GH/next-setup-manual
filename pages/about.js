f6ae93f9d5fe97efbb82d504b6124771a8073d0c
Humberto Asca <hasca@ciplima.org.pe>
2023-02-27 20:44:03 -0500

    Creating the Next.js project for the first time :)

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const about = () => {
+  return (
+    <div>This is the about page about</div>
+  )
+}
+
+export default about
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>Hello world!!! HUMBERTO</div>
+  )
+}
+
+
+export default Home
